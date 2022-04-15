1f0d2703e3dd9d56822cdc628f85fa555fb7b843
riku oshima <riku.oshima@flinters-base.co.jp>
2022-04-15 14:19:38 +0900

    exercise１修正

--- sql/basis/day2/exercise1.sql
+++ sql/basis/day2/exercise1.sql
@@ -1,1 +1,2 @@
-select * from shohin where (hanbai_tanka - shiire_tanka) >= 300;
+select *
+from shohin where (shohin_bunrui = "衣服" OR shohin_bunrui = "キッチン用品") AND (hanbai_tanka - shiire_tanka) >= 300;
