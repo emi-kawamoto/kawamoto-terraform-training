6d56d7e25817393dc5cf6c73a3927f5840a24758
riku oshima <riku.oshima@flinters-base.co.jp>
2022-05-10 18:03:17 +0900

    訂正

--- sql/practical/day1/enshu4.sql
+++ sql/practical/day1/enshu4.sql
@@ -4,7 +4,7 @@ SELECT
     SUM(impressions) AS impressions_total 
 FROM users AS u
     LEFT JOIN activity_reports AS a_r 
-      ON u.id = a_r.user_id 
+        ON u.id = a_r.user_id 
 GROUP BY 
     u.id,
     name
