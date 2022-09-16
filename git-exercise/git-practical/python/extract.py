3cf3e9c6012380be139ede38ef22c146561ce343
t_kanabuchi <toko.kanabuchi@flinters-base.co.jp>
2022-09-16 16:42:41 +0900

    add: files for git additional curriculum

---
+++ git-exercise/git-practical/python/extract.py
@@ -0,0 +1,10 @@
+from pkgutil import ImpImporter
+
+
+import os
+import urllib.request as req
+
+url = 'https://www.jma.go.jp/bosai/forecast/data/forecast/010000.json'
+filename = 'tenki.json'
+dir = 'json'
+req.urlretrieve(url, os.path.join(dir, filename))
