712d2d70f4058853a3dc9bf273cf505976bdcbde
t_kanabuchi <toko.kanabuchi@flinters-base.co.jp>
2022-10-07 08:34:19 +0900

    ref: urlretrieveの廃止

--- git-exercise/git-practical/python/extract.py
+++ git-exercise/git-practical/python/extract.py
@@ -4,4 +4,6 @@ import urllib.request as req
 url = 'https://www.jma.go.jp/bosai/forecast/data/forecast/010000.json'
 filename = 'tenki.json'
 dir = 'json'
-req.urlretrieve(url, os.path.join(dir, filename))
+data = req.urlopen(url).read()
+with open(os.path.join(dir, filename), mode="wb") as f:
+    f.write(data)
