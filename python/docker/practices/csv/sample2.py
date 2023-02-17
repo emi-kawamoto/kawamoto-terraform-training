8589c709135793571f0ddf3b85b2658c181e1567
keiyu harada <keiyu.harada@flinters.co.jp>
2023-02-17 10:44:20 +0900

    fix:拡張子をcsvに修正

--- python/docker/practices/csv/sample2.py
+++ python/docker/practices/csv/sample2.py
@@ -7,7 +7,7 @@ with open(
     next(reader)
 
     with open(
-        "sample_data/result.tsv", newline="", mode="w", encoding="utf-8"
+        "sample_data/result.csv", newline="", mode="w", encoding="utf-8"
     ) as write_file:
         writer = csv.writer(write_file, delimiter="\t")
         writer.writerow(["築年数", "部屋数"])
