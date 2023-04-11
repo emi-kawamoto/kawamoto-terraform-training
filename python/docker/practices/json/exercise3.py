71c2898042f924a5954a4438eb1461f991799899
KengoNakamura6100032 <kengo.nakamura@flinters-base.co.jp>
2023-04-11 11:19:05 +0900

    Fix: 新しい行を追加する処理を追加
    exercise3.pyの9行目に該当の処理を追加

--- python/docker/practices/json/exercise3.py
+++ python/docker/practices/json/exercise3.py
@@ -6,5 +6,6 @@ OUTPUT_FILE_PATH = "sample_data/result.json"
 
 with open(INPUT_FILE_PATH, "r", newline="", encoding="utf-8") as r:
     reader = json.load(r)
-    with open(OUTPUT_FILE_PATH, "w+", encoding="utf-8") as w:
+    reader["banana"] = {"price": 150}
+    with open(OUTPUT_FILE_PATH, "w+", encoding="utf-8") as w:        
         json.dump(reader, w, ensure_ascii=False, indent=2)
