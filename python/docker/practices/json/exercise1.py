d8fd42b694bc0a10c0ce5ed64907e15a3ac28976
KengoNakamura6100032 <129707413+KengoNakamura6100032@users.noreply.github.com>
2023-04-18 13:56:55 +0900

    Revert "Develop/browser"

--- python/docker/practices/json/exercise1.py
+++
@@ -1,8 +0,0 @@
-"""training json program1"""
-import json
-
-FILE_PATH = "sample_data/sample_price.json"
-
-with open(FILE_PATH, "r", encoding="utf-8") as f:
-    reader = json.load(f)
-    print(json.dumps(reader, indent=2, ensure_ascii=False))
