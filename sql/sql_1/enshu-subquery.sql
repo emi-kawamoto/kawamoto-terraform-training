a55d94e7c61ffd80ef0a094b35317ed75f92ffad
emi-kawamoto <e_kawamoto@flinters-base.co.jp>
2024-03-13 16:05:34 +0900

    演習(サブクエリ)の回答

--- sql/sql_1/enshu-subquery.sql
+++ sql/sql_1/enshu-subquery.sql
@@ -1,14 +1,15 @@
 演習1
-SELECT                                                                                       ->   company_id, employee_id, employee_name,
-    ->     (
-    ->     SELECT COUNT(*)
-    ->     FROM
-    ->       children
-    ->     WHERE
-    ->       children.employee_id = employees.employee_id
-    ->     )
-    ->     AS
-    ->       child_cnt
-    ->   FROM
-    ->     employees
-    -> ;
+SELECT
+   company_id, employee_id, employee_name,
+    (
+   SELECT COUNT(*)
+   FROM
+   children
+   WHERE
+   (children.employee_id = employees.employee_id AND children.company_id = employees.company_id)
+   )
+   AS
+   children_cnt
+FROM
+employees
+;
