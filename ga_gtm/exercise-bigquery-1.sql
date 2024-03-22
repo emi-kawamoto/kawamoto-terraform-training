146cf787aef507af6fe8343339e4147bac907648
emi-kawamoto <e_kawamoto@flinters-base.co.jp>
2024-03-22 15:31:37 +0900

    重複を修正

--- ga_gtm/exercise-bigquery-1.sql
+++ ga_gtm/exercise-bigquery-1.sql
@@ -1,6 +1,6 @@
 SELECT
 event_date,
-count(1) AS user_counts
+count(DISTINCT(user_pseudo_id)) AS user_counts
 FROM `flinters-base-education.analytics_282778073.events_*`
 WHERE _TABLE_SUFFIX BETWEEN FORMAT_TIMESTAMP("%Y%m%d",CURRENT_DATE('Asia/Tokyo')-7) AND FORMAT_TIMESTAMP("%Y%m%d",CURRENT_DATE('Asia/Tokyo'))
 AND event_name = 'page_view'
